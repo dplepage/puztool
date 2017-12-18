f9ccdd31bb7ad7e78f1cff60c35d783d1ee6c110
Daniel Lepage <dplepage@gmail.com>
2017-12-17 23:11:06 -0500

    Add core module.

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .parse import pg, pt
+from .words import lists, npl
+from .text import shift, unshift
+from .misc import smoosh, show
+from .qat import qat
+from .modifier import TextModifier, In, deletions, perms
+from . import gridsearch
