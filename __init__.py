06782fd1364e64902cf746a0829c1d77a345881e
Daniel Lepage <dplepage@gmail.com>
2018-01-06 11:47:27 -0500

    Expose more helpers in root namespace.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from .parse import parse_grid, parse_lists, parse_table, pg, pt, pl
 from .words import lists, npl
-from .text import shift, unshift
+from .text import shift, unshift, normalize, lowers
 from .misc import smoosh, show
 from .dropquote import DropQuote
 from .qat import qat
-from .modifier import FnModifier, TextModifier, In, deletions, perms
+from .modifier import FnModifier, TextModifier, In, deletions, perms, Unique
 from . import gridsearch
 from . import logic_grid
 
@@ -19,6 +19,8 @@ __all__ = [
     'npl',
     'shift',
     'unshift',
+    'normalize',
+    'lowers',
     'smoosh',
     'show',
     'DropQuote',
@@ -28,6 +30,7 @@ __all__ = [
     'In',
     'deletions',
     'perms',
+    'Unique',
     'gridsearch',
     'logic_grid',
 ]
