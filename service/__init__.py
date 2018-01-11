bf73797a78d8b25db52b4fbed08887992dca2df0
Daniel Lepage <dplepage@gmail.com>
2018-01-11 13:30:08 -0500

    Add anagram service.

--- service/__init__.py
+++ service/__init__.py
@@ -1,3 +1,4 @@
 from .service import QueryError, StructureChanged, Service, Result
 from .qat import qat
 from .nutrimatic import nutr
+from .wordsmith import wordsmith
---
+++ service/wordsmith.py
@@ -0,0 +1,31 @@
+import re
+from bs4 import BeautifulSoup
+
+from .service import Service, QueryError, StructureChanged
+
+
+class WordsmithService(Service):
+    urlbase = "https://new.wordsmith.org/anagram/anagram.cgi?anagram={{{}}}&t=20"
+    statre = re.compile('(?P<total>\d+) found. Displaying')
+
+    def parse_page(self, query, page):
+        page = BeautifulSoup(page, 'lxml')
+        p = page.select_one(".p402_premium > p")
+        status = p.select_one("b").text
+        stats = self.statre.match(status)
+        if not stats:
+            raise StructureChanged('No header block')
+        total = int(stats.group('total'))
+        entries = ''.join(p.findAll(text=True, recursive=False)).splitlines()
+        return entries, len(entries) < total, total
+
+    def extract_from_table(self, table):
+        for row in table.select('tr'):
+            yield [col.text.strip() for col in row.select('td')]
+
+wordsmith = WordsmithService()
+
+
+if __name__ == '__main__':
+    import sys
+    print(wordsmith(sys.argv[1]))
