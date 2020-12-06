5c4a1935403c8ed27231d1979345250085fbe80c
Daniel Lepage <dplepage@gmail.com>
2020-12-06 15:16:39 -0500

    Improve image and audio tooling.

---
+++ puztool/aud.py
@@ -0,0 +1,37 @@
+import io
+import wave
+import numpy as np
+from IPython import display
+
+def _extract_wav(reader):
+    params = reader.getparams()
+    frames = reader.readframes(params.nframes)
+    print(f"{params.nchannels} channels, {params.sampwidth} bytes/sample")
+    print(f"{params.nframes} frames at rate {params.framerate}")
+    print(f"Returned array is {params.nframes}x{params.nchannels}, <i{params.sampwidth}")
+    data = np.frombuffer(frames, dtype=f'<i{params.sampwidth}')
+    if params.nchannels > 1:
+        return data.reshape((-1, params.nchannels))
+    return data
+
+def load_wav(filename_or_fp):
+    return _extract_wav(wave.open(filename_or_fp))
+
+def parse_wav(raw_bytes):
+    return load_wav(io.BytesIO(raw_bytes))
+
+def disp(data, rate=44100):
+    '''Display audio data in IPython.
+
+    data can be 1D or have shape (nsamples, nchannels)
+    '''
+    return display.Audio(data.T, rate=rate)
+
+def save_wav(data, filename, rate=44100):
+    # ensure it's nsamp x nchannels
+    data = data.reshape((data.shape[0], -1))
+    with wave.open(filename, 'w') as writer:
+        writer.setframerate(rate)
+        writer.setsampwidth(data.dtype.itemsize)
+        writer.setnchannels(data.shape[1])
+        writer.writeframes(data.tobytes())
--- puztool/im.py
+++ puztool/im.py
@@ -1,55 +1,106 @@
+import io
 import os
 import numpy as np
-import png
 from PIL import Image
+import imageio
 
 # This is convenient because it means scipy.misc.imshow will work
 os.environ.setdefault('SCIPY_PIL_IMAGE_VIEWER', 'open -W')
 
-def read_png(fname):
+def _read_png(fname):
     '''Read a PNG into a numpy array.
 
-    PIL can't handle multichannel images with >8 bits, so Image.load(), and thus
-    scipy.misc.imread or imageio.imread, silently truncate the extra bits.
-
-    This function, on the other hand, will correctly read a 16-bit png.
+    See https://github.com/imageio/imageio/issues/352
     '''
-    w, h, data, info = png.Reader(fname).read()
-    data = np.array(list(data))
-    return data.reshape(h, w, info['planes'])
+    return imageio.imread(fname, format="PNG-FI")
 
-def read(fname, format=None):
-    if format == 'png' or (format is None and fname.endswith("png")):
-        return read_png(fname)
-    return np.array(Image.open(fname))
+def _info(data):
+    h, w, *d = data.shape
+    if len(d) > 1:
+        raise Exception("Image has too many dimensions?")
+    try:
+        depth = data.dtype.itemsize*8
+        if not d or d[0] == 1:
+            mode = 'grayscale'
+        elif d[0] == 3:
+            mode = 'RGB'
+        elif d[0] == 4:
+            mode = 'RGBA'
+        else:
+            mode = f'{d[0]}-channel'
+        if not d:
+            shape = f'{h}x{w}'
+        else:
+            shape = f'{h}x{w}x{d[0]}'
+        print(f"{w}x{h} {depth}-bit {mode} image (array is {shape})")
+    except Exception as e:
+        print(f"Failed info for image: {e}")
+    return data
 
-def to(data, norm=False, zoom=1):
+def read(fname, fmt=None, dtype=None):
+    if fmt == 'png' or (fmt is None and fname.endswith("png")):
+        data = _read_png(fname)
+    else:
+        data = imageio.imread(fname)
+    if dtype is not None:
+        dtype = np.dtype(dtype)
+        if dtype.kind == 'b':
+            data = (data > 0).astype(dtype)
+        elif dtype.kind in 'fc' and data.dtype.kind in 'iu':
+            data = normalize(data)
+    return _info(data)
+
+def normalize(data):
+    '''Rescale data to [0,1].
+
+    The result is always floating-point
+    '''
+    m, M = data.min(), data.max()
+    if m == M:
+        print("Warning: normalizing uniform data")
+        return np.ones(data.shape)
+    return ((data*1.0)-m)/(M-m)
+
+norm = normalize
+
+def parse(byts):
+    if isinstance(byts, np.ndarray):
+        byts = byts.tobytes()
+    return _info(imageio.imread(io.BytesIO(byts)))
+
+def paste():
+    try:
+        return _info(imageio.imread("<clipboard>"))
+    except RuntimeError as e:
+        print(f"Error: {e}")
+        return None
+
+def disp(data, norm=False, zoom='auto'):
     if data.dtype == bool:
-        img = (data*255).astype('uint8')
+        data = (data*255).astype('uint8')
     else:
         if norm:
-            data = (data-data.min())*1.0/data.max()
-        if data.max() < 1:
-            img = (data*255).astype('uint8')
+            data = normalize(data)
+        if data.max() <= 1:
+            data = (data*255).astype('uint8')
         elif data.max() < 256:
-            img = data.astype('uint8')
+            data = data.astype('uint8')
         else:
-            img = (data*255.0/data.max()).astype('uint8')
-    im = Image.fromarray(img)
+            data = (data*255.0/data.max()).astype('uint8')
+    img = Image.fromarray(data)
+    if zoom == 'auto':
+        w, h = img.width, img.height
+        wscale = 700/img.width
+        hscale = 500/img.height
+        zoom = min(wscale, hscale)
     if zoom != 1:
-        im = im.resize((int(im.width*zoom), int(im.height*zoom)), Image.NEAREST)
-    return im
+        neww, newh = int(img.width*zoom), int(img.height*zoom)
+        print(f"Scaled {img.width}x{img.height} img to {neww}x{newh}")
+        img = img.resize((neww, newh), Image.NEAREST)
+    return img
+
+# def ndisp(data, zoom=1):
+# TODO: remember what this function was going to be?
 
 def show(data, norm=False):
-    return to(data, norm).show()
-
-def subrect(data, mask=None, pad=0):
-    '''Return a sub-rectangle of data containing everywhere mask is True.'''
-    if mask is None:
-        mask = data
-    rows, cols, *rest = np.where(mask)
-    if rest:
-        raise ValueError("Mask must be 2D")
-    rm, rM = rows.min(), rows.max()
-    cm, cM = cols.min(), cols.max()
-    return data[rm-pad:rM+1+pad,cm-pad:cM+1+pad]
+    return disp(data, norm).show()
